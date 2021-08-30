45cf50b3b2251cc09165df386cfe1a05179f8c88
Praveen Varma <swamiraju.varma@learner.manipal.edu>
2021-08-30 11:24:43 +0530

    modules

--- new.js
+++ new.js
@@ -7,4 +7,14 @@ myEmitter.on("speak", function (mssg) {
 });
 myEmitter.emit("speak", "Hello Peter!");
 
+var Person = function (name) {
+  this.name = name;
+};
 
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person("James Rhodes");
+james.on("say", function (mssg) {
+  console.log(`${james.name} said : ${mssg}`);
+});
+james.emit("say", "Boom you looking for this!");
